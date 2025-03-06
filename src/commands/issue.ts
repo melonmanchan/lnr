f22d42f9bc1a53fa18239fd58e3811e8b9ee0aa4
Matti Jokitulppo <matti.jokitulppo@metacoregames.com>
2025-03-06 10:54:35 +0200

    Add linear client

--- src/commands/issue.ts
+++ src/commands/issue.ts
@@ -1,9 +1,15 @@
 import { command, subcommands } from "cmd-ts";
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const list = command({
   name: "list",
   args: {},
   handler: async () => {
+    console.log("list1");
+    await delay(1000);
+
     console.log("list");
   },
 });
---
+++ src/linear/client.ts
@@ -0,0 +1,13 @@
+import { LinearClient, type LinearClientOptions } from "@linear/sdk";
+
+const LINEAR_API_KEY = process.env.LINEAR_API_KEY;
+const LINEAR_OAUTH_TOKEN = process.env.LINEAR_OAUTH_TOKEN;
+
+const options: LinearClientOptions = {
+  apiKey: LINEAR_API_KEY,
+  accessToken: LINEAR_OAUTH_TOKEN,
+};
+
+const client = new LinearClient(options);
+
+export default client;
