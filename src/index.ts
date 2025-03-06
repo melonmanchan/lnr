8333e0f3d6df7d061d800bbe230838ca5afa13b0
Matti Jokitulppo <matti.jokitulppo@metacoregames.com>
2025-03-06 10:24:21 +0200

    Add cmd-ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { command, run, string, positional } from "cmd-ts";
+
+const app = command({
+  name: "my-first-app",
+  args: {
+    someArg: positional({ type: string, displayName: "some arg" }),
+  },
+  handler: ({ someArg }) => {
+    console.log({ someArg });
+  },
+});
+
+run(app, process.argv.slice(2));
