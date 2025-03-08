cb2bc1299f10710a32b7121134d8ca99448e290f
Matti Jokitulppo <matti.jokitulppo@metacoregames.com>
2025-03-08 15:28:15 +0200

    Read version from package.json

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import { run, subcommands } from "cmd-ts";
 import { issue } from "./commands/issue.ts";
 import { linearInitialized } from "./linear/client.ts";
 
+import packageJson from "../package.json" with { type: "json" };
+
 if (!linearInitialized()) {
   console.log(1);
   process.exit(0);
 }
 
 const app = subcommands({
+  version: packageJson.version,
   name: "lr",
   cmds: { issue },
 });
