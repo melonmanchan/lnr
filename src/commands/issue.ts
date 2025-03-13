6563bb594f90d3a0bc33aa54e12240c263e06b22
Matti Jokitulppo <matti.jokitulppo@metacoregames.com>
2025-03-13 16:29:32 +0200

    Better cycle API

--- src/commands/issue.ts
+++ src/commands/issue.ts
@@ -24,7 +24,10 @@ import { openTextEditor } from "../console/editor.ts";
 import { Issue, Project, Team } from "@linear/sdk";
 import process from "node:process";
 import { getConfig } from "../config/config.ts";
-import type { IssuesQueryVariables } from "@linear/sdk/dist/_generated_documents.d.ts";
+import type {
+  IssuesQueryVariables,
+  NullableCycleFilter,
+} from "@linear/sdk/dist/_generated_documents.d.ts";
 
 const issueStates = [
   "canceled",
@@ -37,6 +40,10 @@ const issueStates = [
 
 type IssueState = (typeof issueStates)[number];
 
+const cycleStates = ["active", "previous", "next"];
+
+type CycleState = (typeof issueStates)[number];
+
 const stateColors: { [key: IssueState]: ChalkInstance } = {
   canceled: chalk.red,
   completed: chalk.green,
@@ -44,6 +51,20 @@ const stateColors: { [key: IssueState]: ChalkInstance } = {
   unstarted: chalk.yellow,
   backlog: chalk.magenta,
   triage: chalk.cyan,
+};
+
+const getCycleFilter = (cycle: CycleState): { cycle: NullableCycleFilter } => {
+  switch (cycle) {
+    case "active":
+      return { cycle: { isActive: { eq: true } } };
+    case "previous":
+      return { cycle: { isNext: { eq: true } } };
+    case "next":
+      return { cycle: { isPrevious: { eq: true } } };
+
+    default:
+      return { cycle: {} };
+  }
 };
 
 const list = command({
@@ -57,12 +78,11 @@ const list = command({
         "Filter by issue state (completed, canceled, backlog, triage, unstarted, started). Default is everything except completed or cancelled",
     }),
 
-    activeCycle: flag({
-      type: boolean,
-      long: "active-cycle",
-      short: "ac",
-      defaultValue: () => false,
-      description: "Display only issues in the current active cycle",
+    cycle: option({
+      type: optional(oneOf<IssueState>(cycleStates)),
+      long: "cycle",
+      short: "c",
+      description: "Cycle filters (current, previous, next)",
     }),
 
     assignee: option({
@@ -81,7 +101,7 @@ const list = command({
     }),
   },
 
-  handler: async ({ state, assignee, project, activeCycle }) => {
+  handler: async ({ state, assignee, project, cycle }) => {
     const config = await getConfig();
     const client = getLinearClient(config.linearApiKey);
 
@@ -97,9 +117,7 @@ const list = command({
           : { assignee: { isMe: { eq: true } } }
         : { assignee: { displayName: { containsIgnoreCase: assignee } } };
 
-    const cycleFilter = activeCycle
-      ? { cycle: { isActive: { eq: true } } }
-      : {};
+    const cycleFilter = cycle ? getCycleFilter(cycle) : {};
 
     const filter: IssuesQueryVariables["filter"] = {
       ...stateFilter,
